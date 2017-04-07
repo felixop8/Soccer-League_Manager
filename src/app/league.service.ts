24c2ddadfe7a29310c23c19b61dc2278e6c453b4
felixop8 <felixop8@gmail.com>
2017-04-07 09:14:42 -0700

    retrieve data from firebase into league service

--- src/app/league.service.ts
+++ src/app/league.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Injectable()
 export class LeagueService {
+  teams: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private angularFire: AngularFire) {
+    this.teams = angularFire.database.list('info');
+  }
+
+  getTeams(){
+    return this.teams;
+  }
 
 }
