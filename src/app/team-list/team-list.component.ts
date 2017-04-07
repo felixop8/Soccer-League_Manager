86f03d4be5cecac7c1f8374a4c7f2270bba93607
felixop8 <felixop8@gmail.com>
2017-04-07 09:27:19 -0700

    add service into onInit to retrieve data automatically

--- src/app/team-list/team-list.component.ts
+++ src/app/team-list/team-list.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { LeagueService } from '../league.service';
 
 @Component({
   selector: 'app-team-list',
   templateUrl: './team-list.component.html',
-  styleUrls: ['./team-list.component.css']
+  styleUrls: ['./team-list.component.css'],
+  providers: [LeagueService]
+
 })
 export class TeamListComponent implements OnInit {
   teams: FirebaseListObservable<any[]>;
 
 
-  constructor() { }
+  constructor(private leagueService: LeagueService) { }
 
   ngOnInit() {
+    this.teams = this.leagueService.getTeams(); 
   }
 
 }
