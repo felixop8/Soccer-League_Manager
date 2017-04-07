a32b7d660fb9a2760680e2e2fbf1771da58b796f
felixop8 <felixop8@gmail.com>
2017-04-07 09:46:51 -0700

    create route entry for team:id

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,17 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TeamListComponent } from './team-list/team-list.component';
+import { TeamComponent } from './team/team.component';
 
 
 const appRoutes: Routes = [
   {
       path: '',
       component: TeamListComponent
+  },
+  {
+      path: 'team/:id',
+      component: TeamComponent
   }
 
 ];
