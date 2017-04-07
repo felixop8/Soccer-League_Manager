23fb3b363609555234e5c45fa47405c3f013d4d7
felixop8 <felixop8@gmail.com>
2017-04-07 13:16:55 -0700

    retrieve params from team

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
+import { FirebaseObjectObservable } from 'angularfire2';
+import { LeagueService } from '../league.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  providers: [LeagueService]
 })
 export class AboutComponent implements OnInit {
   teamId: any;
   private sub: any;
+  aboutToDisplay;
 
-  constructor(private route: ActivatedRoute, private location: Location, private router: Router) { }
+  constructor(private route: ActivatedRoute, private location: Location, private leagueService: LeagueService, private router: Router) { }
 
   ngOnInit() {
     this.sub = this.route.parent.params.subscribe(params => {
             this.teamId = +params["id"];
     });
-    console.log(this.teamId);
+
+    this.aboutToDisplay = this.leagueService.getTeamById(this.teamId);
   }
 
 }
