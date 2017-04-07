e035b779aa8628f28c899dd538e1b82268cfac0f
felixop8 <felixop8@gmail.com>
2017-04-07 14:33:29 -0700

    add route for board Members

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -21,6 +21,10 @@ const appRoutes: Routes = [
         {
            path: 'players',
            component: PlayersComponent
+        },
+        {
+           path: 'boardMembers',
+           component: BoardMembersComponent
         }
       ]
   }
