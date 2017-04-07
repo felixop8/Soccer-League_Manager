a8a81db78f83e06b905514a0ab122e45a64d6d58
felixop8 <felixop8@gmail.com>
2017-04-07 08:42:24 -0700

    create file api-keys and add credentials

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+    apiKey: "AIzaSyC9aKU5ywpEKdAQVqDsodfKtnDR5aAp5tY",
+    authDomain: "epicodus-fc.firebaseapp.com",
+    databaseURL: "https://epicodus-fc.firebaseio.com",
+    storageBucket: "epicodus-fc.appspot.com",
+    messagingSenderId: "768670806576"
+  };
