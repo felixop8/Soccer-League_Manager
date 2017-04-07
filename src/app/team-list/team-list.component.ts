fe2c8f6f0af35eb641e27f6a03d9092d57f53ce0
felixop8 <felixop8@gmail.com>
2017-04-07 09:17:44 -0700

    add league:firebaseObservable into team-list component

--- src/app/team-list/team-list.component.ts
+++ src/app/team-list/team-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-team-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team-list.component.css']
 })
 export class TeamListComponent implements OnInit {
+  teams: FirebaseListObservable<any[]>;
+
 
   constructor() { }
 
