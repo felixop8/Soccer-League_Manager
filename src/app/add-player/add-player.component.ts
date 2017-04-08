d95c0ab6f65284874df43217193be76bafdfc070
felixop8 <felixop8@gmail.com>
2017-04-08 16:33:48 -0700

    create method to add players

--- src/app/add-player/add-player.component.ts
+++ src/app/add-player/add-player.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { LeagueService } from '../league.service';
 import { Router } from '@angular/router';
+import { Players } from '../players';
 
 @Component({
   selector: 'app-add-player',
@@ -22,5 +23,19 @@ export class AddPlayerComponent implements OnInit {
   onChange(optionFromMenu) {
   this.teamSelected = optionFromMenu;
   console.log(this.teamSelected);
+
 }
+
+submitForm(name: string, bio: string, position: string, photoProfile: string) {
+  const newPlayer: Players = new Players (
+    name,
+    bio,
+    position,
+    photoProfile
+  )
+  this.leagueService.addPlayer(newPlayer, this.teamSelected);
+
+
+}
+
 }
--- src/app/league.service.ts
+++ src/app/league.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { Team } from './team';
+import { Players } from './players';
 
 @Injectable()
 export class LeagueService {
   teams: FirebaseListObservable<any[]>;
-
+  allTeams;
+  reference;
   constructor(private angularFire: AngularFire) {
     this.teams = angularFire.database.list('teams');
   }
@@ -22,4 +24,21 @@ export class LeagueService {
    this.teams.push(newTeam);
  }
 
-}
+ addPlayer(newPlayer: Players, teamSelected){
+   this.teams.subscribe(dataLastEmittedFromObserver => {
+     this.allTeams = dataLastEmittedFromObserver;
+     var keyHolder = Object.keys(this.allTeams);
+
+
+    //  for (var i = 0; i < keyHolder.length; i++) {
+    //    if (keyHolder[i] == teamSelected) {
+         var reference = this.angularFire.database.object('/teams/'+ keyHolder[1] + '/players/');
+         reference.remove();
+
+
+    //    }
+    //  }
+
+   });
+  }
+ }
