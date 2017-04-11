e9c35afa619ea791c1f35621b70fb6e8f114bcb5
felixop8 <felixop8@gmail.com>
2017-04-10 18:54:33 -0700

    add snapshot for each boardmembers

--- src/app/board-members/board-members.component.ts
+++ src/app/board-members/board-members.component.ts
@@ -4,6 +4,7 @@ import { Location } from '@angular/common';
 import { FirebaseObjectObservable } from 'angularfire2';
 import { LeagueService } from '../league.service';
 import { Router } from '@angular/router';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-board-members',
@@ -14,15 +15,23 @@ import { Router } from '@angular/router';
 export class BoardMembersComponent implements OnInit {
   teamId;
   boardMembersToDisplay;
+    myItems: any[] = [];
 
 
-  constructor(private route: ActivatedRoute, private location: Location, private leagueService: LeagueService, private router: Router) { }
+  constructor(private route: ActivatedRoute, private location: Location, private leagueService: LeagueService, private router: Router, private angularFire: AngularFire) { }
 
   ngOnInit() {
     this.route.parent.params.forEach((urlParameters) =>  {
             this.teamId = urlParameters['id'];
     });
     this.boardMembersToDisplay = this.leagueService.getTeamById(this.teamId);
+
+    this.angularFire.database.list('/teams/' + [this.teamId] + '/boardMembers', { preserveSnapshot: true})
+    .subscribe(snapshots=>{
+        snapshots.forEach(snapshot => {
+          this.myItems.push(snapshot.val());
+        });
+    })
   }
 
 }
