6ab1812f427b5de9dce84c44679150ff21fba6d2
felixop8 <felixop8@gmail.com>
2017-04-14 15:57:29 -0700

    delete extra info

--- src/app/api-keys.ts
+++
@@ -1,7 +0,0 @@
-export let masterFirebaseConfig = {
-    apiKey: 'AIzaSyC9aKU5ywpEKdAQVqDsodfKtnDR5aAp5tY',
-    authDomain: 'epicodus-fc.firebaseapp.com',
-    databaseURL: 'https://epicodus-fc.firebaseio.com',
-    storageBucket: 'epicodus-fc.appspot.com',
-    messagingSenderId: '768670806576'
-  };
